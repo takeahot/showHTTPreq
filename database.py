622c8b0fe52b7997efe0bb18d7ce675faa31d7a2
Anton Arestov <mirtrudmaynow@gmail.com>
2024-04-27 15:18:27 +0530

    once again

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-URL_DATABASE = 'postgresql://postgres:PoAn!000@localhost:5432/clay_pigeon_DB'
+import os, pprint
+env = os.environ
+# os.environ['DATABASE_URL'] = 'postgresql://postgres:PoAn!000@localhost:5432/clay_pigeon_DB'
+pprint.pprint(dict(env), width= 1 );
+# URL_DATABASE = os.environ.get('DATABASE_URL') 
 
 engine = create_engine(URL_DATABASE)
 
