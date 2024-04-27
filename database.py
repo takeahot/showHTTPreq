75dbb55822b286a07dffeb24b9559cceaf31172b
Anton Arestov <mirtrudmaynow@gmail.com>
2024-04-27 23:47:02 +0530

    delete import pprint

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os, pprint
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
