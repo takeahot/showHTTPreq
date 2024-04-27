3f8ae919cf4f5f4f1e880900c237e48ea89be8b8
Anton Arestov <mirtrudmaynow@gmail.com>
2024-04-28 00:42:38 +0530

    reorganize url string for make db engine

--- database.py
+++ database.py
@@ -1,14 +1,24 @@
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
+import os, pprint
 from dotenv import load_dotenv
 
 load_dotenv()
 
 env = os.environ
-URL_DATABASE = os.environ.get('DATABASE_URL').replace("s://", "sql://")
-
+connect_kwargs = dict(
+    username=env['DATABASE_USER'],
+    password=env['DATABASE_PASSWORD'],
+    host=env['DATABASE_HOST'],
+    database=env['DATABASE_NAME'],
+)
+if env['DATABASE_PORT']:
+    connect_kwargs['port'] = env['DATABASE_PORT']
+URL_DATABASE = URL.create(
+    'postgresql',
+    **connect_kwargs
+)
 print('url_database', URL_DATABASE)
 
 engine = create_engine(URL_DATABASE)
