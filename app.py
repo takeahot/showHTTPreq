56261353254ec897e904ecf8305cb025dbaae80a
Anton Arestov <mirtrudmaynow@gmail.com>
2024-04-27 20:40:14 +0530

    remake to logs working, not finished (meke env var, comment quiz)

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-class ChoiceBase(BaseModel):
-    choice_text: str
-    is_correct: bool
+# class ChoiceBase(BaseModel):
+#     choice_text: str
+#     is_correct: bool
 
-class QuestionBase(BaseModel):
-    question_text: str
-    choices: List[ChoiceBase]
+# class QuestionBase(BaseModel):
+#     question_text: str
+#     choices: List[ChoiceBase]
 
 def get_db():
     db = SessionLocal()
@@ -32,16 +32,18 @@ async def read_question(question_id: int, db: db_dependency):
         raise HTTPException(status_code=404, detail='Question is not found')
     return result
 
-@app.post("/questions/")
-async def create_questions(question: QuestionBase, db: db_dependency):
-    db_question = models.Questions(question_text=question.question_text)
-    db.add(db_question)
-    db.commit()
-    db.refresh(db_question)
-    for choice in question.choices:
-        db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
-        db.add(db_choice)
-    db.commit()
+# @app.post("/questions/")
+# async def create_questions(question: QuestionBase, db: db_dependency):
+#     db_question = models.Questions(question_text=question.question_text)
+#     db.add(db_question)
+#     db.commit()
+#     db.refresh(db_question)
+#     for choice in question.choices:
+#         db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
+#         db.add(db_choice)
+#     db.commit()
+
+# @app.post('')
 
 # app.get("/")
 # def read_root():
--- models.py
+++ models.py
@@ -1,17 +1,23 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from database import Base
 
-class Questions(Base):
-    __tablename__ = 'questions'
+# class Questions(Base):
+#     __tablename__ = 'questions'
 
-    id = Column(Integer, primary_key=True, index=True)
-    question_text = Column(String, index=True)
+#     id = Column(Integer, primary_key=True, index=True)
+#     question_text = Column(String, index=True)
 
-class Choices(Base):
-    __tablename__ = 'choices'
+# class Choices(Base):
+#     __tablename__ = 'choices'
 
-    id = Column(Integer, primary_key=True, index=True)
-    choice_text = Column(String, index=True)
-    is_correct = Column(Boolean, default=False)
-    question_id = Column(Integer, ForeignKey("questions.id"))
+#     id = Column(Integer, primary_key=True, index=True)
+#     choice_text = Column(String, index=True)
+#     is_correct = Column(Boolean, default=False)
+#     question_id = Column(Integer, ForeignKey("questions.id"))
 
+class Logs(Base):
+    __tablename__ = 'logs'
+
+    id = Column(Integer, unique=True, primary_key=True, index=True)
+    timestamp = Column(String, index=True)
+    log = Column(String, index=True)
