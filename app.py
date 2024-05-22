8029f3b9fd0cd72bd0da1c7051fe843c0bfa0f47
Anton Arestov <mirtrudmaynow@gmail.com>
2024-05-23 02:44:11 +0530

    add route reqest from clay-pigeon to ELMA

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, HTTPException, Depends, Request, Body
 from typing import List, Annotated
 from pydantic import BaseModel
 from database import engine, SessionLocal
-import models, json, pprint, datetime
+import models, json, pprint, datetime, httpx
 from sqlalchemy.orm import Session
 from sqlalchemy import text
+from functools import reduce
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -61,24 +62,55 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.api_route('/',methods=['GET','PUT','POST','DELETE','PATCH','HEAD','OPTIONS','TRACE','CONNECT'])
 async def write_request(request: Request, db: db_dependency):
     # return json.dump(Request.method)
+    bodyObj = await request.json()
     db_logs = models.Logs(
         timestamp= datetime.datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z"), 
         httpmethod=request.method, 
         headers=json.dumps(request.headers.__dict__, cls=CustomJSONEncoder), 
-        body=json.dumps(await request.json()),
+        body=json.dumps(bodyObj),
         path_params=repr(request.path_params), 
         query_params=json.dumps(request.query_params.__dict__)
     )
     db.add(db_logs)
     db.commit()
     db.refresh(db_logs)
-    pprint.pp ({
-        'method': request.method, 
-        "headers": request.headers, 
-        "body":await request.json(), 
-        "path_params": request.path_params, 
-        "query_params": request.query_params
-    })
+    if str(bodyObj['eventName']) not in [\
+        'ticket_created',\
+        'ticket_updated',\
+        'ticket_comment_created',\
+        'ticket_comment_updated'\
+    ]:
+        print ('request got')
+    else:
+    # Отправка запроса на внешний URL
+        async with httpx.AsyncClient() as client:
+            external_response = await client.request(
+                # method=request.method,
+                url="https://7isfa26wfvp4a.elma365.eu/api/"  
+                + "extensions/22fe87c3-14fc-4c97-83dd-52ef65fa4644/script/"
+                + bodyObj['eventName'],
+                headers=request.headers,
+                json=bodyObj
+            )
+
+        # Сохранение ответа от внешнего URL в БД
+        db_response_logs = models.Logs(
+            timestamp= datetime.datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z"), 
+            httpmethod=request.method, 
+            headers=json.dumps(dict(external_response.headers), cls=CustomJSONEncoder),
+            body=json.dumps(dict({\
+                'status_code':external_response.status_code},\
+                **{'payload': {'text': external_response.text}\
+            })),
+            path_params=repr(request.path_params), 
+            query_params=json.dumps(request.query_params.__dict__),
+        )
+        db.add(db_response_logs)
+        db.commit()
+        db.refresh(db_response_logs)
+
+        print('Request and response logged')
+
     return {
         'method': request.method, 
         "headers": request.headers, 
@@ -110,9 +142,44 @@ async def show_requests(db: db_dependency):
         return dict1
 
     result = db.query(models.Logs).all()
-    # print(str(result[0].__dict__))
-    print(type(result))
+
     result = list(map(lambda x: immutableDictUpdate(immutableDictUpdate(x.__dict__,json.loads(x.body)),x.payload) if 'payload' in x.body else x ,result))
+    # result = db.query(models.Logs).statement.columns.keys()
+    # result = tableModel
+    if not result: 
+        raise HTTPException(status_code=404, detail='Logs is not found')
+    return result
+
+@app.api_route('/present_casavi_body/{page_str}',methods=['GET','PUT','POST','DELETE','PATCH','HEAD','OPTIONS','TRACE','CONNECT'])
+async def show_requests(page_str, db: db_dependency):
+
+    pageSize = 1000
+
+    def immutableDictUpdate(dict1,dict2):
+        dict1.update(dict2)
+        return dict1
+
+    page = int(page_str)
+
+    dbanswer = db.query(models.Logs).filter( models.Logs.id <= (page * pageSize),models.Logs.id > ((page - 1) * pageSize) ).all()#.where(models.Logs.id > (page * 1000 - 1000)).all()
+    # dbanswer = [{ 'a': 1 , "body": '{"payload":{"b":1,"c":2}}'}]
+
+    def flatDbAnswerItem(item):
+        if 'payload' in item['body']: 
+            bodyUpped = immutableDictUpdate(item,json.loads(item['body']))
+            payloadUpped = immutableDictUpdate(bodyUpped ,item['payload'])
+            return payloadUpped
+        else:
+            return item
+    unsortedResult = list(map( flatDbAnswerItem , [ans.__dict__ for ans in dbanswer] ))
+    
+    def sortResultItem (item):
+        return dict(sorted(item.items(), key=lambda answerItem: answerItem[0]))
+    
+    headers = {v: v for v in list(reduce(lambda allKeys, dict: allKeys.union(dict.keys()),unsortedResult,set()))}
+    unsortedResult.insert(0,headers)
+    result = list(map(sortResultItem, unsortedResult))
+
     # result = db.query(models.Logs).statement.columns.keys()
     # result = tableModel
     if not result: 
