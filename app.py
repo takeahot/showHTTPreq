ea9f9d7fdf1751cb6b9350bfeeb203838b2a14f5
Anton Arestov <mirtrudmaynow@gmail.com>
2024-05-23 23:22:57 +0530

    debug problem with request from koyeb

--- app.py
+++ app.py
@@ -61,89 +61,94 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.api_route('/',methods=['GET','PUT','POST','DELETE','PATCH','HEAD','OPTIONS','TRACE','CONNECT'])
 async def write_request(request: Request, db: db_dependency):
-    # return json.dump(Request.method)
-    bodyObj = await request.json()
-    db_logs = models.Logs(
-        timestamp= datetime.datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z"), 
-        httpmethod=request.method, 
-        headers=json.dumps(request.headers.__dict__, cls=CustomJSONEncoder), 
-        body=json.dumps(bodyObj),
-        path_params=repr(request.path_params), 
-        query_params=json.dumps(request.query_params.__dict__)
-    )
-    db.add(db_logs)
-    db.commit()
-    db.refresh(db_logs)
-    if str(bodyObj['eventName']) not in [\
-        'ticket_created',\
-        'ticket_updated',\
-        'ticket_comment_created',\
-        'ticket_comment_updated'\
-    ]:
-        print ('request got',str(bodyObj['eventName']))
-    else:
-    # Отправка запроса на внешний URL
-        # Преобразование заголовков в словарь
-        headers_dict = dict(request.headers)
-        headers_dict.pop('content-length', None)
-        headers_dict.pop('host', None)
-
-        async with httpx.AsyncClient() as client:
-            print(
-                dict({
-                    'method': request.method,
-                    'url': "https://7isfa26wfvp4a.elma365.eu/api/"  
-                    + "extensions/22fe87c3-14fc-4c97-83dd-52ef65fa4644/script/"
-                    + bodyObj['eventName'],
-                    'headers': request.headers,
-                    'json': bodyObj
-                })
-            )
-            external_response = await client.request(
-                method=request.method,
-                url="https://7isfa26wfvp4a.elma365.eu/api/"  
-                + "extensions/22fe87c3-14fc-4c97-83dd-52ef65fa4644/script/"
-                + bodyObj['eventName'],
-                headers=headers_dict,
-                json=bodyObj
-            )
-
-
-        response_data = dict()
-
-        if not (200 <= external_response.status_code < 300):
-            response_data['payload'] = {
-                'textError': f"data:text/html,{external_response.text}"
-            }
-        else: 
-            response_data = external_response.json()
-
-        response_data['status_code'] = external_response.status_code
-        response_data['eventName'] = bodyObj['eventName'] + "_response"
-
-        # Сохранение ответа от внешнего URL в БД
-        db_response_logs = models.Logs(
+    try:
+        # return json.dump(Request.method)
+        bodyObj = await request.json()
+        db_logs = models.Logs(
             timestamp= datetime.datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z"), 
             httpmethod=request.method, 
-            headers=json.dumps(dict(external_response.headers), cls=CustomJSONEncoder),
-            body=json.dumps(response_data),
+            headers=json.dumps(request.headers.__dict__, cls=CustomJSONEncoder), 
+            body=json.dumps(bodyObj),
             path_params=repr(request.path_params), 
-            query_params=json.dumps(request.query_params.__dict__),
+            query_params=json.dumps(request.query_params.__dict__)
         )
-        db.add(db_response_logs)
+        db.add(db_logs)
         db.commit()
-        db.refresh(db_response_logs)
+        db.refresh(db_logs)
+        if str(bodyObj['eventName']) not in [\
+            'ticket_created',\
+            'ticket_updated',\
+            'ticket_comment_created',\
+            'ticket_comment_updated'\
+        ]:
+            print ('request got',str(bodyObj['eventName']))
+        else:
+        # Отправка запроса на внешний URL
+            # Преобразование заголовков в словарь
+            headers_dict = dict(request.headers)
+            headers_dict.pop('content-length', None)
+            headers_dict.pop('host', None)
 
-        print('Request and response logged')
-        print(response_data)
+            timeout = httpx.Timeout(30.0, connect=10.0, read=30.0)
+            async with httpx.AsyncClient() as client:
+                print(
+                    dict({
+                        'method': request.method,
+                        'url': "https://7isfa26wfvp4a.elma365.eu/api/"  
+                        + "extensions/22fe87c3-14fc-4c97-83dd-52ef65fa4644/script/"
+                        + bodyObj['eventName'],
+                        'headers': request.headers,
+                        'json': bodyObj
+                    })
+                )
+                external_response = await client.request(
+                    method=request.method,
+                    url="https://7isfa26wfvp4a.elma365.eu/api/"  
+                    + "extensions/22fe87c3-14fc-4c97-83dd-52ef65fa4644/script/"
+                    + bodyObj['eventName'],
+                    headers=headers_dict,
+                    json=bodyObj
+                )
 
-    return {
-        'method': request.method, 
-        "headers": request.headers, 
-        "body":await request.json(), 
-        "path_params": request.path_params, 
-        "query_params": request.query_params
-    }
+
+            response_data = dict()
+
+            if not (200 <= external_response.status_code < 300):
+                response_data['payload'] = {
+                    'textError': f"data:text/html,{external_response.text}"
+                }
+            else: 
+                response_data = external_response.json()
+
+            response_data['status_code'] = external_response.status_code
+            response_data['eventName'] = bodyObj['eventName'] + "_response"
+
+            # Сохранение ответа от внешнего URL в БД
+            db_response_logs = models.Logs(
+                timestamp= datetime.datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z"), 
+                httpmethod=request.method, 
+                headers=json.dumps(dict(external_response.headers), cls=CustomJSONEncoder),
+                body=json.dumps(response_data),
+                path_params=repr(request.path_params), 
+                query_params=json.dumps(request.query_params.__dict__),
+            )
+            db.add(db_response_logs)
+            db.commit()
+            db.refresh(db_response_logs)
+
+            print('Request and response logged')
+            print(response_data)
+
+        return {
+            'method': request.method, 
+            "headers": request.headers, 
+            "body":await request.json(), 
+            "path_params": request.path_params, 
+            "query_params": request.query_params
+        }
+    except Exception as e:
+        print(f"Error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.api_route('/list',methods=['GET','PUT','POST','DELETE','PATCH','HEAD','OPTIONS','TRACE','CONNECT'])
 async def show_requests(db: db_dependency):
