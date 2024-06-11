2881b505289718669ef78119efec5f15877a457f
Anton Arestov <mirtrudmaynow@gmail.com>
2024-06-11 19:29:21 +0530

    начали возвращать  database.py и main чтобы подключения работали

--- database.py
+++ database.py
@@ -1,9 +1,28 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import URL
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# Загрузка переменных окружения из файла .env
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+env = os.environ
+connect_kwargs = dict(
+    username=env['DATABASE_USER'],
+    password=env['DATABASE_PASSWORD'],
+    host=env['DATABASE_HOST'],
+    database=env['DATABASE_NAME'],
+)
+if 'DATABASE_PORT' in env:
+    connect_kwargs['port'] = env['DATABASE_PORT']
+
+URL_DATABASE = URL.create(
+    'postgresql',
+    **connect_kwargs
+)
+
+engine = create_engine(URL_DATABASE, pool_pre_ping=True, pool_recycle=300)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import logs, requests
+from sqlalchemy import text
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+# Вывод схемы таблицы (для PostgreSQL или другой базы данных)
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'logs'"))
+    print(result.fetchall())
 
 app.include_router(logs.router)
 app.include_router(requests.router)
