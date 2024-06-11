57e98911d66597921029c4a073d6ab1c2358a906
Anton Arestov <mirtrudmaynow@gmail.com>
2024-06-12 02:53:06 +0530

    поправил ответ, елси запро  не пересылвается

--- routers/requests.py
+++ routers/requests.py
@@ -18,39 +18,41 @@ domains = [
 
 @router.api_route('/', methods=['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS', 'TRACE', 'CONNECT'])
 async def write_request(request: Request, db: Session = Depends(get_db)):
-    async with limiter:
-        try:
-            bodyObj = await request.json()
-            db_logs = models.Logs(
-                timestamp=datetime.datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z"),
-                httpmethod=request.method,
-                headers=json.dumps(dict(request.headers), cls=CustomJSONEncoder),
-                body=json.dumps(bodyObj),
-                path_params=repr(request.path_params),
-                query_params=json.dumps(dict(request.query_params))
-            )
-            db.add(db_logs)
-            db.commit()
-            db.refresh(db_logs)
+    try:
+        bodyObj = await request.json()
+        db_logs = models.Logs(
+            timestamp=datetime.datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z"),
+            httpmethod=request.method,
+            headers=json.dumps(dict(request.headers), cls=CustomJSONEncoder),
+            body=json.dumps(bodyObj),
+            path_params=repr(request.path_params),
+            query_params=json.dumps(dict(request.query_params))
+        )
+        db.add(db_logs)
+        db.commit()
+        db.refresh(db_logs)
 
-            if str(bodyObj.get('eventName')) not in [
-                'ticket_created',
-                'ticket_updated',
-                'ticket_comment_created',
-                'ticket_comment_updated'
-            ]:
-                print('Request received:', str(bodyObj['eventName']))
-            else:
-                print('Request received for resend:', str(bodyObj['eventName']))
+        if str(bodyObj.get('eventName')) not in [
+            'ticket_created',
+            'ticket_updated',
+            'ticket_comment_created',
+            'ticket_comment_updated'
+        ]:
+            print('Request received:', str(bodyObj['eventName']))
+            return bodyObj; 
+        else:
+            print('Request received for resend:', str(bodyObj['eventName']))
 
-                headers_dict = dict(request.headers)
-                headers_dict.pop('content-length', None)
-                headers_dict.pop('host', None)
+            headers_dict = dict(request.headers)
+            headers_dict.pop('content-length', None)
+            headers_dict.pop('host', None)
 
-                elma_tail = bodyObj['eventName'];
-                bodyObj = {**bodyObj,'eventName': f"{elma_tail}_from_koyeb_to_ELMA"}
+            elma_tail = bodyObj['eventName'];
+            bodyObj = {**bodyObj,'eventName': f"{elma_tail}_from_koyeb_to_ELMA"}
 
-                for domain in domains:
+            response_datas = []
+            for domain in domains:
+                async with limiter:
                     async with httpx.AsyncClient() as client:
                         try:
                             print(
@@ -114,8 +116,8 @@ async def write_request(request: Request, db: Session = Depends(get_db)):
                         except httpx.RequestError as exc:
                             print(f"An error occurred while requesting {exc.request.url!r}.")
                             raise HTTPException(status_code=500, detail=f"Request to external service failed: {str(exc)}")
-
-            return response_data
-        except Exception as e:
-            print(f"Error occurred: {str(e)}")
-            raise HTTPException(status_code=500, detail=str(e))
+                response_datas = response_datas + [response_data]
+            return response_datas
+    except Exception as e:
+        print(f"Error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
