b32003c38aaf4804b213ce4adac2a345de8fd929
Anton Arestov <mirtrudmaynow@gmail.com>
2024-07-02 03:39:34 +0700

    борюсь с переносами строки, решил это сделать на последнем этапе при записи в файл при помощи сериализатора, но он почемуто не запускается, логирую

--- routers/logs.py
+++ routers/logs.py
@@ -11,7 +11,6 @@ import logging
 from pyrsistent import pmap
 from pprint import pformat
 
-
 router = APIRouter()
 
 # Настройка логирования
@@ -23,8 +22,13 @@ logging.basicConfig(filename=log_file_path, level=logging.INFO,
                     format='%(asctime)s %(levelname)s: %(message)s')
 
 def default_serializer(obj):
+    logging.info(f'Serializing object: {obj}')
     if isinstance(obj, datetime):
         return obj.isoformat()
+    if isinstance(obj, str):
+        serialized_obj = obj.replace('\n', '\\u000A')
+        logging.info(f'Serialized string: {serialized_obj}')
+        return serialized_obj
     return str(obj)
 
 @router.get("/logs", response_model=List[schemas.Log], operation_id="read_logs")
@@ -50,8 +54,16 @@ async def logs_parsed_by_page(page_str: int, db: Session = Depends(get_db)):
     logging.info(f'Запрос на страницу: {page_str}')
     page = int(page_str)
     logging.info(f'Номер страницы: {page}')
-    
-    dbanswer = db.query(models.Logs).filter(models.Logs.id <= (page * pageSize), models.Logs.id > ((page - 1) * pageSize)).all()
+
+    # Получаем список столбцов
+    columns = models.Logs.__table__.columns.keys()
+
+    # Формируем запрос, используя только определенные столбцы
+    dbanswer = db.query(*[getattr(models.Logs, col) for col in columns]) \
+                .filter(models.Logs.id <= (page * pageSize), models.Logs.id > ((page - 1) * pageSize)) \
+                .all()
+
+    dbanswer = [{col: getattr(row, col) for col in columns} for row in dbanswer]
     logging.info(f'Количество записей на странице: {len(dbanswer)}')
     
     if not dbanswer:
@@ -61,29 +73,37 @@ async def logs_parsed_by_page(page_str: int, db: Session = Depends(get_db)):
     # Логирование исходных данных из базы
     logging.info('Исходные данные из базы данных:')
     for row in dbanswer:
-        logging.info(json.dumps(row.__dict__, default=default_serializer, ensure_ascii=False, indent=4))
+        logging.info(json.dumps(row, default=default_serializer, indent=4))
     
     def flatDbAnswerItem(item: Dict[str, Any]) -> Dict[str, Any]:
-        logging.info(f'Обработка item: {json.dumps(item, default=default_serializer, ensure_ascii=False, indent=4)}')
+        logging.info(f'Обработка item: {json.dumps(item, default=default_serializer, indent=4)}')
         
+        # Логирование всех свойств item с их типами данных
+        for key, value in item.items():
+            logging.info(f'Ключ: {key}, Значение: {value}, Тип данных: {type(value)}')
+            
         if 'payload' in item['body']:
             logging.info(f'Найден payload в body: {item["body"]}')
+            
             bodyUpped = pmap(item).update(json.loads(item['body']))
             bodyUpped_dict = dict(bodyUpped)
-            logging.info(f'Результат после обновления bodyUpped: {json.dumps(bodyUpped_dict, default=default_serializer, ensure_ascii=False, indent=4)}')
+            logging.info(f'Результат после обновления bodyUpped: {json.dumps(bodyUpped_dict, default=default_serializer, indent=4)}')
             
             payloadUpped = bodyUpped.update(bodyUpped['payload'])
             payloadUpped_dict = dict(payloadUpped)
-            logging.info(f'Результат после обновления payloadUpped: {json.dumps(payloadUpped_dict, default=default_serializer, ensure_ascii=False, indent=4)}')
+            logging.info(f'Результат после обновления payloadUpped: {json.dumps(payloadUpped_dict, default=default_serializer, indent=4)}')
+            
+            # Логирование всех свойств после обновления payload
+            for key, value in payloadUpped_dict.items():
+                logging.info(f'Ключ: {key}, Значение: {value}, Тип данных: {type(value)}')
             
             return payloadUpped_dict
         else:
             return item
 
-
     unsortedResult = []
     for ans in dbanswer:
-        item = ans.__dict__.copy()  # Используем копию, чтобы избежать изменений оригинала
+        item = ans.copy()  # Используем копию, чтобы избежать изменений оригинала
         flat_item = flatDbAnswerItem(item)
         unsortedResult.append(flat_item)
     
@@ -110,8 +130,8 @@ async def logs_parsed_by_page(page_str: int, db: Session = Depends(get_db)):
     response_log_file_path = os.path.join(log_dir, f"response_page_{page}.txt")
     
     with open(response_log_file_path, "w", encoding="utf-8") as log_file:
-        log_file.write(json.dumps(result, ensure_ascii=False, indent=4, default=default_serializer))
+        log_file.write(json.dumps(result, indent=4, default=default_serializer))
     
     logging.info(f'Ответ записан в файл: {response_log_file_path}')
     
-    return json.loads(json.dumps(result, ensure_ascii=False, indent=4, default=default_serializer))
+    return json.loads(json.dumps(result, indent=4, default=default_serializer))
