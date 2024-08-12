0aba1bb180e7f9922a6ac95b5b4c68107e4dabdd
Anton Arestov <mirtrudmaynow@gmail.com>
2024-08-13 00:26:48 +0700

    stop send message to any instance of ELMA

--- routers/requests.py
+++ routers/requests.py
@@ -14,8 +14,8 @@ from fastapi.responses import FileResponse
 router = APIRouter()
 
 domains = [
-    "https://morzhkzdhj3oi.elma365.eu",
-    "https://7isfa26wfvp4a.elma365.eu"
+    # "https://morzhkzdhj3oi.elma365.eu",
+    # "https://7isfa26wfvp4a.elma365.eu"
 ]
 
 ALLOWED_BROWSERS = ["chrome", "opera", "firefox", "safari", "edge"]
